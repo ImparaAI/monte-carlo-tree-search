1820857fc84de9d9ee03b74dd5044abd3d08b27b
Jan Hartigan <janhartigan13@gmail.com>
2018-09-14 18:16:23 -0700

    Including readme and license files in python package distribution (#4)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="imparaai-montecarlo",
-    version="1.0.2",
+    version="1.0.3",
     license='MIT',
     author="ImparaAI",
     author_email="author@example.com",
