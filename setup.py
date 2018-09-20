7c9e8dfae1806da335165254bf431f49ea4e0df9
Jan Hartigan <janhartigan13@gmail.com>
2018-09-20 15:21:47 -0700

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="imparaai-montecarlo",
-    version="1.1.1",
+    version="1.1.2",
     license='MIT',
     author="ImparaAI",
     author_email="author@example.com",
