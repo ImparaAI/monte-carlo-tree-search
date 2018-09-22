70add3e498464761b65a8c12fcda4d43fcf5ac43
Sergio Somma <sdsomma@gmail.com>
2018-09-21 23:58:45 -0700

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="imparaai-montecarlo",
-    version="1.2.0",
+    version="1.2.1",
     license='MIT',
     author="ImparaAI",
     author_email="author@example.com",
